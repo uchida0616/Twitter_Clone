8cd3b8a81897a9b44665b5c805f254f90f3442be
yuya uchida <u.yuya0616ll@gmail.com>
2020-12-29 21:43:40 +0900

    paramsメゾットの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,13 @@
 class TweetsController < ApplicationController
+
   def index
   end
+
   def new
     @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(title: params[:tweet][:title], content: params[:tweet][:content])
   end
 end
